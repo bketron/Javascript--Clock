18eb02812184ed1c07dbef63f17cf75b92c9e0fb
Brennen <ketron@outlook.com>
2017-03-15 11:37:56 -0700

    completed the normal and hard modes of the javascript clock assignment

---
+++ main.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+	
+	function time(){
+
+		setTimeout(function(){
+
+		var d = new Date();
+		var hour = d.getHours()
+		var min = d.getMinutes()
+		var sec = d.getSeconds()
+		var amPm = ""
+		var colorHex = ""
+
+
+		if(hour < 10) {
+			hour = "0" + hour
+		}
+
+		if(hour > 12) {
+			hour = hour - 12
+			amPm = "p.m."
+		}
+
+		if(hour < 12) {
+			hour = hour
+			amPm = "a.m."
+		}
+
+		if(min < 10) {
+			min = "0" + min
+		}
+
+		if(sec < 10) {
+			sec = "0" + sec
+		}
+
+		colorHex = hour.toString() + min.toString() + sec.toString()
+		colorHex = parseInt(colorHex)
+
+		document.getElementById("timer").innerHTML = hour + ":" + min + ":" + sec + " " + amPm
+		document.body.style.backgroundColor = `#${colorHex}`
+
+		time()
+
+	},1000)
+
+
+	}
+
+	time()
+
+
+
+})
